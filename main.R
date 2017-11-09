d93ad32b85bbac17f03e80c47c631156673a474a
Gonçalo Nogueira <jgoncalonogueira@gmail.com>
2017-11-09 21:59:21 +0000

    Split sentences and write them to file

--- main.R
+++ main.R
@@ -1,34 +1,69 @@
 library(tm)
 library(openNLP)
+library(rlist)
 
-# Uncomment the following lines if you wish to work with a smaller subset
-# of the data for experimenting purposes
-corpus <- VCorpus(DirSource("data/en_US/sandbox", encoding = "UTF-8"),
-                                readerControl = list(language = "en_US"))
 
-# Uncomment the following lines if you wish to work with the full dataset
-# corpus <- PCorpus(DirSource("data/en_US", encoding = "UTF-8"),
-#                                 readerControl = list(language = "en_US"),
-#                                 dbControl = list(dbName = "en_US.db",
-#                                                  dbType = "DB1"))
+# Set this constant to TRUE if you want to work with a smaller dataset
+# for experimenting purposes
+# Set this constant to FALSE to work with the full dataset
+SANDBOX <- TRUE
 
-replace.non.ascii.punctuation <- function(x) {
-  y <- gsub("[‘’]", "'", x)
-  y <- gsub("[“”]", '"', y)
-  y <- gsub("…", "...", y)
+if (SANDBOX) {
+    base.dir <- "data/en_US/sandbox"
+} else {
+    base.dir <- "data/en_US"
 }
 
-corpus <- tm_map(corpus, content_transformer(replace.non.ascii.punctuation))
-corpus <- tm_map(corpus, removeNumbers)
-corpus <- tm_map(corpus, stripWhitespace)
-profanity_list <- readLines("data/en_US/profanity_list.txt")
-corpus <- tm_map(corpus, removeWords, profanity_list)
-rm(profanity_list)
+get_clean_corpus <- function(sandbox = FALSE) {
 
-sentences_by_line <- list()
-sentences_by_line$blogs <- content(corpus[["en_US.blogs.txt"]])
-sentences_by_line$news <- content(corpus[["en_US.news.txt"]])
-sentences_by_line$twitter <- content(corpus[["en_US.twitter.txt"]])
+    if (!file.exists(paste(base.dir, "clean/en_US.blogs.txt", sep = "/"))) {
+
+        if (sandbox) {
+            corpus <- VCorpus(DirSource(base.dir , encoding = "UTF-8"),
+                                        readerControl = list(language = "en_US"))
+        } else {
+            corpus <- PCorpus(DirSource(base.dir, encoding = "UTF-8"),
+                                        readerControl = list(language = "en_US"),
+                                        dbControl = list(dbName = "en_US.db",
+                                                         dbType = "DB1"))
+        }
+
+        replace.non.ascii.punctuation <- function(x) {
+          y <- gsub("[‘’]", "'", x)
+          y <- gsub("[“”]", '"', y)
+          y <- gsub("…", "...", y)
+        }
+
+        corpus <- tm_map(corpus, content_transformer(replace.non.ascii.punctuation))
+        corpus <- tm_map(corpus, removeNumbers)
+        corpus <- tm_map(corpus, stripWhitespace)
+        profanity_list <- readLines("data/en_US/profanity/profanity_list.txt")
+        corpus <- tm_map(corpus, removeWords, profanity_list)
+        rm(profanity_list)
+
+        clean.dir <- paste(base.dir, "clean", sep = "/")
+        dir.create(clean.dir)
+        writeCorpus(corpus, path=clean.dir,
+                    c("en_US.blogs.txt",
+                      "en_US.news.txt",
+                      "en_US.twitter.txt")
+                    )
+    } else {
+        clean.dir <- paste(base.dir, "clean", sep = "/")
+        if (sandbox) {
+            corpus <- VCorpus(DirSource(clean.dir , encoding = "UTF-8"),
+                                        readerControl = list(language = "en_US"))
+        } else {
+            corpus <- PCorpus(DirSource(clean.dir, encoding = "UTF-8"),
+                              readerControl = list(language = "en_US"),
+                              dbControl = list(dbName = "en_US.db",
+                                               dbType = "DB1"))
+        }
+    }
+    corpus
+}
+
+corpus <- get_clean_corpus(sandbox = SANDBOX)
 
 
 sent_token_annotator <- Maxent_Sent_Token_Annotator()
@@ -38,16 +73,53 @@ split_sentences <- function(x) {
   x[y]
 }
 
-sentences <- list()
-for (i in seq_along(sentences_by_line)) {
-  for (j in seq_along(sentences_by_line[[i]])) {
-    sents <- split_sentences(sentences_by_line[[i]][j])
-    if (j == 1) {
-      sentences[[i]] <- sents
-    } else {
-      sentences[[i]] <- c(sentences[[i]], sents)
-    }
-  }
+# This removes all punctuation except for apostrophes
+# This exception is important in order to keep the contractions
+# typical of the english language (e.g. let's, can't)
+remove_punctuation <- function(x) {
+  gsub("(?!')[[:punct:]]", "", x, perl = TRUE)
 }
-names(sentences) <- c("blogs", "news", "twitter")
-rm(sentences_by_line)
+ 
+insert_sent_annotations <- function(x) {
+  paste("<s>", x, "</s>")
+}
+
+clean_sentence <- function(x) {
+    y <- remove_punctuation(x)
+    y <- trimws(y)
+    y <- insert_sent_annotations(y)
+}
+
+ngrams_of_sentence <- function(sentence, n) {
+  tokens_sent <- scan_tokenizer(sentence)
+  vapply(ngrams(tokens_sent, n), paste, "", collapse = " ")
+}
+
+write_sentences <- function(line, sentences.file) {
+    sents <- split_sentences(line)
+    sents <- sapply(sents, clean_sentence)
+    write(sents, sentences.file)
+}
+
+write_ngrams <- function(sentence,
+                         unigrams.file,
+                         bigrams.file,
+                         trigrams.file) {
+     tokens <- scan_tokenizer(sentence[i])
+     write(tokens, unigrams.file)
+     write(ngrams_of_sentence(sentence, 2), bigrams.file)
+     write(ngrams_of_sentence(sentence, 3), trigrams.file)
+}
+
+dir.create(paste(base.dir, "sentences", sep = "/"))
+
+if (!file.exists(paste(base.dir, "sentences/sentences.txt", sep = "/"))) {
+    sentences <- file(paste(base.dir, "sentences/sentences.txt", sep = "/"), "w")
+
+    for (doc in seq_along(corpus)) {
+        for (line in seq_along(content(corpus[[doc]]))) {
+            line <- content(corpus[[doc]])[line]
+            write_sentences(line, sentences)
+        }
+    }
+}
