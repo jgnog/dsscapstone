6227df8ecffb3f17eb2e9346e8db8ef6229a4838
Gonçalo Nogueira <jgoncalonogueira@gmail.com>
2018-02-08 15:24:06 +0000

    Temporary commit

--- model.R
+++ model.R
@@ -14,33 +14,130 @@ if (SANDBOX) {
     base.dir <- "data/en_US"
 }
 
+# unigrams <- readLines(paste(base.dir, "ngrams/unigrams.txt", sep = "/"))
+# words_table <- table(unigrams)
+# words_table <- sort(words_table, decreasing = TRUE)
+# # Keeping the environment as lean as possible
+# rm(unigrams)
+
+# replace_with_unk <- function(word) {
+#     if (word %in% names(words_table)) {
+#         "<UNK>"
+#     } else {
+#         word
+#     }
+# }
+
+# TODO: generalize trigram matrix for bigrams and unigrams
 unigrams <- readLines(paste(base.dir, "ngrams/unigrams.txt", sep = "/"))
-words_table <- table(unigrams)
-words_table <- sort(words_table, decreasing = TRUE)
-# Keeping the environment as lean as possible
-rm(unigrams)
-
-replace_with_unk <- function(word) {
-    if (word %in% names(words_table)) {
-        "<UNK>"
-    } else {
-        word
-    }
-}
-
-
+bigrams <- readLines(paste(base.dir, "ngrams/bigrams.txt", sep = "/"))
 trigrams <- readLines(paste(base.dir, "ngrams/trigrams.txt", sep = "/"))
 
 # TODO: remove line for smaller dataset
 # Work with a smaller dataset for experimentation purposes
+unigrams <- unigrams[1:5000]
+bigrams <- bigrams[1:5000]
 trigrams <- trigrams[1:5000]
 
-tokenized_trigrams <- sapply(trigrams, scan_tokenizer, USE.NAMES = FALSE)
-first_bigram_in_trigrams <- apply(tokenized_trigrams[1:2,],
-                                  2, paste, collapse = " ")
-third_words <- tokenized_trigrams[3,]
-unique_bigrams <- unique(first_bigram_in_trigrams)
-unique_next_words <- unique(third_words)
+ngrams <- list(unigrams = unigrams,
+               bigrams = bigrams,
+               trigrams = trigrams)
+
+rm(unigrams, bigrams, trigrams)
+
+decompose_ngram <- function(ngram) {
+# This function decomposes an ngram into a two elements list: the first
+# contains all the words except the last and the second element contains the last word.
+#
+# ngram   the character vector containing the ngram to be decomposed
+    words <- scan_tokenizer(ngram)
+    n_words <- length(words)
+    # For the first element get all except the last element of vector words
+    # and then paste the words together with a space between them.
+    # For the second element simply get the last word in the vector words
+    list(paste(words[1:n_words - 1], collapse = " "),
+         words[n_words])
+}
+
+
+
+decomposed_ngrams <- list(bigrams = transpose(ngrams$bigrams %>% map(decompose_ngram)),
+                          trigrams = transpose(ngrams$trigrams %>% map(decompose_ngram)))
+decomposed_ngrams <- list(unigrams = ngrams$unigrams,
+                          bigrams = list(precedents = flatten_chr(decomposed_ngrams$bigrams[[1]]),
+                                         subsequents = flatten_chr(decomposed_ngrams$bigrams[[2]])),
+                          trigrams = list(precedents = flatten_chr(decomposed_ngrams$trigrams[[1]]),
+                                          subsequents = flatten_chr(decomposed_ngrams$trigrams[[2]])))
+
+decomposed_ngrams$unigrams <- unique(unigrams)
+decomposed_ngrams$bigrams <- list(precedents = unique(decomposed_ngrams$bigrams$precedents),
+                                  subsequents = unique(decomposed_ngrams$bigrams$subsequents))
+decomposed_ngrams$trigrams <- list(precedents = unique(decomposed_ngrams$trigrams$precedents),
+                                   subsequents = unique(decomposed_ngrams$trigrams$subsequents))
+
+
+
+ngram_mt <- list(unigrams = decomposed_ngrams$unigrams,
+                 bigrams = Matrix(data = 0,
+                                  nrow = length(decomposed_ngrams$bigrams$precedents),
+                                  ncol = length(decomposed_ngrams$bigrams$subsequents),
+                                  dimnames = list(decomposed_ngrams$bigrams$precedents,
+                                                  decomposed_ngrams$bigrams$subsequents)),
+                 trigrams = Matrix(data = 0,
+                                   nrow = length(decomposed_ngrams$trigrams$precedents),
+                                   ncol = length(decomposed_ngrams$trigrams$subsequents),
+                                   dimnames = list(decomposed_ngrams$trigrams$precedents,
+                                                   decomposed_ngrams$trigrams$subsequents)))
+
+rm(decomposed_ngrams)
+
+generate_hash <- function(v) {
+    hashmap(v, 1:length(v))
+}
+
+matrices_hashes <- list(unigrams = generate_hash(ngram_mt$unigrams),
+                        bigrams = list(precedents = generate_hash(rownames(ngram_mt$bigrams)),
+                                       precedents = generate_hash(colnames(ngram_mt$bigrams))),
+                        trigrams = list(precedents = generate_hash(rownames(ngram_mt$trigrams)),
+                                        precedents = generate_hash(colnames(ngram_mt$trigrams))))
+
+get_matrix_indices <- function(precedent, subsequent) {
+
+    nr_of_words_in_precedent <- length(scan_tokenizer(precedent))
+
+    if (nr_of_words_in_precedent == 2) {
+        type_of_ngram <- "trigram"
+    } else if (nr_of_words_in_precedent == 1) {
+        type_of_ngram <- "bigram"
+    } else {
+        return(NULL)
+    }
+
+    rows_hash <- matrices_hashes[[type_of_ngram]][['precedents']]
+    cols_hash <- matrices_hashes[[type_of_ngram]][['subsequents']]
+    
+    i <- rows_hash[[precedent]] 
+    j <- cols_hash[[subsequent]] 
+
+    c(i, j)
+}
+
+for (i in seq_along(ngram_mt)) {
+
+
+    
+
+
+
+                          
+
+
+# tokenized_trigrams <- sapply(trigrams, scan_tokenizer, USE.NAMES = FALSE)
+# first_bigram_in_trigrams <- apply(tokenized_trigrams[1:2,],
+#                                   2, paste, collapse = " ")
+# third_words <- tokenized_trigrams[3,]
+# unique_bigrams <- unique(first_bigram_in_trigrams)
+# unique_next_words <- unique(third_words)
 
 trigram_matrix <- Matrix(data = 0,
                          nrow = length(unique_bigrams),
